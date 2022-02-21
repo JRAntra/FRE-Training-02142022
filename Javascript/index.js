fa58b8a4e44c0ccd7b397028fe80a9139b665488
show3567 <ydong011@fiu.edu>
2022-02-21 15:33:45 -0600

    update

--- Javascript/index.js
+++ Javascript/index.js
@@ -126,10 +126,68 @@
 // bird.move();
 // monkey.move();
 
-// const arr = [];
-
-// Array.prototype.myForEach = function() {
-//     console.log('this is myforeach!');
+// // loop
+// const arr = [1, 2, 3];
+// const obj = {
+//     name: 'Jojo',
+//     age: 18
+// }
+// for (const key in obj) {
+//     console.log(typeof key);
+// }
+// for (const num of arr) {
+//     console.log(num);
 // }
 
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] === 2) {
+//        break;
+//     }
+//     console.log(arr[i]);
+// }
 
+Array.prototype.myForEach = function (callback) {
+    for (let i = 0; i < this.length; i++) {
+        callback(this[i], i, this);
+    }
+};
+
+// arr.myForEach((num, i, array) => {
+//     array[i] = num + 1;
+// });
+
+// const arr123 = [true, 1, 'test'];
+// arr123.myForEach();
+
+// const arr = [1, 2, 3];
+
+// Array.prototype.myMap = function (callback) {
+//     const res = [];
+//     for (let i = 0; i < this.length; i++) {
+//         res.push(callback(this[i], i, this));
+//     }
+//     return res;
+// };
+
+// const arr1 = arr.myMap((num, i, array) => {
+//     return num + 2 * 4 - 213;
+// });
+// console.log(arr1);
+
+const arr = [1, 2, 32];
+
+Array.prototype.myFilter = function (callback) {
+    const res = [];
+    for (let i = 0; i < this.length; i++) {
+        if (callback(this[i], i, this)) {
+            res.push(this[i]);
+        }
+    }
+    return res;
+};
+
+const arr1 = arr.myFilter((num, i, array) => {
+    return num > 4;
+});
+
+console.log(arr1);
