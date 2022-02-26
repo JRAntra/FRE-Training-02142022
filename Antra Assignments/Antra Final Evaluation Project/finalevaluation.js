22f4b8f7873c8e00285152819afe59025e2e5c22
Joel Garcia <jzild08@gmail.com>
2022-02-25 21:18:38 -0500

    Finale Evaluation Update

--- Antra Assignments/Antra Final Evaluation Project/finalevaluation.js
+++ Antra Assignments/Antra Final Evaluation Project/finalevaluation.js
@@ -1,5 +1,32 @@
 const url = "https://raw.githubusercontent.com/Show3567/json-server/master/db.json";
 
+let mId = [];
+let mImg = [];
+let mName = [];
+let mOutline = [];
+
+function extractMovies(data) {
+    for (let x = 0; x < data.length; x++){
+        mId.push(data[x].id)
+        mName.push(data[x].name);
+        mImg.push(data[x].imgUrl);
+        mOutline.push(data[x].outlineInfo);
+    } 
+}
+
+
+function printMovies() {
+    for (let a = 0; a < mImg.length; a++) {
+        let newDiv = document.createElement("div");
+
+        document.getElementById("movie__cont").appendChild(newDiv);
+        newDiv.style.width = "100%";
+        newDiv.style.margin = "0 auto";
+        newDiv.style.padding= "0 1em";
+        newDiv.setAttribute("id", "'" + mId[a] + "'");
+        newDiv.innerHTML = "<img src=" + "'" + mImg[a] + "'" + ">" + "<p>" + mName[a] + "</p>" + "<p>" + mOutline[a] + "</p>";        
+    }
+}
 
 
 const getMovies = () => {
@@ -7,37 +34,29 @@ const getMovies = () => {
         .then((response) => response.json())
         .then(data => {
             
+             
             extractMovies(data.movies);
-            
-            
+            printMovies();
         });
 };
 
-const img = [];
-let movieName = [];
-const outline = [];
-
-
-function extractMovies (movie) {
-
-    for (let x = 0; x < movie.length; x++){
-        
-        img.push(movie[x].imgUrl);
-        movieName.push(movie[x].name);
-        outline.push(movie[x].outlineInfo);
-    }
-    
-}
-
-const printMovieDirectory = () =>{
-    for (let a = 0; a < movieName.length; a++){
-        document.getElementsByClassName("movie__box").innerHTML="hello";
-    }
-}
+getMovies();
 
 
 
-document.getElementsByClassName("movie_box").innerHTML = "hello";
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
