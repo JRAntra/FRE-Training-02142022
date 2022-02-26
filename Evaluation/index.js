85eaf22288cfd5113545df3dbd7014387747c1e5
Nathaniel Norris <you@example.comnnorris23@gmail.com>
2022-02-25 23:43:51 -0500

    evaluation

--- Evaluation/index.js
+++ Evaluation/index.js
@@ -1,97 +1,97 @@
+const Api = (() => {
+  const baseUrl = "http://localhost:3000";
 
-// const Api = (() => {
-//   const baseUrl = "http://localhost:3000/posts";
+  const path = "movies";
 
-//   const path = "movies";
+  const getMovies = () =>
+    fetch([baseUrl, path].join("/")).then((response) => response.json());
 
-//   const getMovies = () =>
-//     fetch([baseUrl, path].join("/")).then((response) => response.json());
+  return {
+    getMovies,
+  };
+})();
 
-//   return {
-//     getMovies,
-//   };
-// })();
-
-// // ~~~~~~~~~~~~~View~~~~~~~~~~~~~
-// const View = (() => {
-//   const domstr = {
-//     movieList: ".carousel_box",
+// ~~~~~~~~~~~~~View~~~~~~~~~~~~~
+const View = (() => {
+  const domstr = {
+    movie: ".carousel_box",
    
-//   };
-//   const render = (ele, tmp) => {
-//     ele.innerHTML = tmp;
-//   };
-//   const createTmp = (arr) => {
-//     let tmp = "";
-//     arr.forEach((movie) => {
-//       tmp += `
-//       "id": 1,
-//       "imgUrl": "https://static-wmwm.warnermediacdn.com/styles/img_342_width/s3/2021-12/Untitled%20design%20%2821%29_2.png?itok=w_EnPK-m",
-//       "name": "All Elite Wrestling: Rampage",
-//       "outlineInfo": "New episode Friday at 10 p.m. ET on TNT"    
-//             `;
-//     });
-//     return tmp;
-//   };
-
-//   return {
-//     domstr,
-//     render,
-//     createTmp,
-//   };
-// })();
-
-// const Model = ((api, view) => {
-//   class Todo {
-//     constructor(title) {
-//       (this.userId = 2), (this.title = title), (this.completed = false);
-//     }
-//   }
-//   class State {
-//     #movieList = [];
-
-//     get movieList() {
-//       return this.#movieList;
-//     }
-//     set movieList(newtodolist) {
-//       this.#todolist = newtodolist;
-
-//       const tmp = view.createTmp(this.#movieList);
-//       const ele = document.querySelector(view.domstr.movieList);
-//       view.render(ele, tmp);
-    
-//       const getMovies = api.getMovies;
-
-//       return {
-//         getMovies,
-        
-//       };
-//     })(Api, View);
-
-//     // ~~~~~~~~~~~~~Controller~~~~~~~~~~~~~
-//     const Controller = ((model, view) => {
-//       const state = new model.State();
-//       const movieList = document.querySelector(view.domstr.movieList);
-    
-//         });
+  };
+  const render = (ele, tmp) => {
+    ele.innerHTML = tmp;
+  };
+  const createTmp = (arr) => {
+    let tmp = "";
+    arr.forEach((movie) => {
+      tmp += `
+      <div class="movie">
+              
+            </div>
       
-    
-    
-    
-//       const init = () => {
-//         model.getMovies().then((movies) => {
-//           state.movieList = movies;
-//         });
-//       };
-    
-//       const bootstrap = () => {
-//         init();
-        
-//       };
-    
-//       return { bootstrap };
-//     })(Model, View);
-    
-//     Controller.bootstrap();
+            `;
+    });
+    return tmp;
+  };
 
-//   }
+  return {
+    domstr,
+    render,
+    createTmp,
+  };
+})();
+
+//~~~~~~~~model~~~~~~~~~~~~~~~~~~~~
+
+const Model = ((api, view) => {
+  class Movie {
+    constructor(name) {
+      (this.userId = 2), (this.name = name)
+    }
+  }
+  class State {
+    #movie = [];
+
+    get movie() {
+      return this.#movie;
+    }
+    set movie(newMovie) {
+      this.#movie = newMovie;
+
+      const tmp = view.createTmp(this.#movie);
+      const ele = document.querySelector(view.domstr.movie);
+      view.render(ele, tmp);
+
+    }
+  }
+
+      const getMovies = api.getMovies;
+
+      return {
+        getMovies,
+        State,
+        Movie
+
+      };
+    })(Api, View);
+
+    // ~~~~~~~~~~~~~Controller~~~~~~~~~~~~~
+    const Controller = ((model, view) => {
+      const state = new model.State();
+      const movie = document.querySelector(view.domstr.movie);
+  
+      const init = () => {
+          model.getMovies().then((movies) => {
+              state.movie = movies;
+          });
+      };
+  
+      const bootstrap = () => {
+          init();
+      };
+  
+      return { bootstrap };
+  })(Model, View);
+  
+  Controller.bootstrap();
+
+
