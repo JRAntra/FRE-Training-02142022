5fe573d0ce960696c05ebf244eaadc9fb6366a32
Juvenal Gonzalez <juvgonzalez@my365.bellevue.edu>
2022-02-25 18:01:56 -0800

    Update api.js

--- evaluation/api.js
+++ evaluation/api.js
@@ -1,6 +1,6 @@
 export const Api = (() => {
     const baseUrl = "http://localhost:3000";
-    const path = "pictures";
+    const path = "movies";
 
     const getPictures = () =>
         fetch([baseUrl, path].join("/")).then((response) => response.json());
